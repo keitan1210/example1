8b21ae90b90f3ad0969732153d3217aac932cfad
Kei <124319373+keitan1210@users.noreply.github.com>
2023-09-06 13:32:38 +0900

    Create API.py

---
+++ main/field/API.py
@@ -0,0 +1,15 @@
+import requests
+
+#落書き
+'''
+url='https://www.procon.gr.jp/matches'
+
+params={}
+params['token']='toba09b9dd2c4d03c57affee30e44f44c143957119447bfcc6f6437b77380fa3'
+
+response=requests.get(url,params)
+
+print(response)
+#print(response.json())
+
+'''
